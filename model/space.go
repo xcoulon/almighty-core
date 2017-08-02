ea3922f52aa593099a0b5cd528528e64f0359af5
Xavier Coulon <xcoulon@redhat.com>
2017-08-02 14:47:36 +0200

    Add relationships and meta for backlog
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- model/space.go
+++ model/space.go
@@ -9,35 +9,42 @@ import (
 
 //Space the Space type of resource to (un)marshall in the JSON-API requests/responses
 type Space struct {
-	ID          string `jsonapi:"primary,spaces"`
-	Version     int    `jsonapi:"attr,version"`
-	Name        string `jsonapi:"attr,name"`
-	Description string `jsonapi:"attr,description"`
+	ID          string     `jsonapi:"primary,spaces"`
+	Name        string     `jsonapi:"attr,name"`
+	Description string     `jsonapi:"attr,description"`
+	BackLog     []WorkItem `jsonapi:"relation,backlog"`
+	BackLogSize int        // carried in this struct to be exposed as the `meta/count` attribute in the `workitems`` links
 }
 
+// JSONAPILinks returns the links to the space
 func (s Space) JSONAPILinks() *jsonapi.Links {
 	config := configuration.Get()
 	return &jsonapi.Links{
 		"self": jsonapi.Link{
 			Href: fmt.Sprintf("%[1]s/api/spaces/%[2]s", config.GetAPIServiceURL(), s.ID),
 		},
-		"workitems": jsonapi.Link{
-			Href: fmt.Sprintf("%[1]s/api/spaces/%[2]s", config.GetAPIServiceURL(), s.ID),
-			Meta: map[string]interface{}{
-				"counts": map[string]uint{
-					"likes": 4,
-				},
-			},
-		},
 	}
 }
 
-// Invoked for each relationship defined on the Space struct when marshaled
-// func (s Space) JSONAPIRelationshipLinks(relation string) *jsonapi.Links {
-// 	if relation == "workitems" {
-// 		return &jsonapi.Links{
-// 			"related": fmt.Sprintf("https://example.com/spaces/%s/workitems", s.ID),
-// 		}
-// 	}
-// 	return nil
-// }
+//JSONAPIRelationshipLinks is invoked for each relationship defined on the Space struct when marshaled
+func (s Space) JSONAPIRelationshipLinks(relation string) *jsonapi.Links {
+	config := configuration.Get()
+	if relation == "backlog" {
+		return &jsonapi.Links{
+			"related": jsonapi.Link{
+				Href: fmt.Sprintf("%[1]s/api/spaces/%[2]s/backlog", config.GetAPIServiceURL(), s.ID),
+			},
+		}
+	}
+	return nil
+}
+
+// JSONAPIRelationshipMeta Invoked for each relationship defined on the Post struct when marshaled
+func (s Space) JSONAPIRelationshipMeta(relation string) *jsonapi.Meta {
+	if relation == "backlog" {
+		return &jsonapi.Meta{
+			"totalCount": s.BackLogSize,
+		}
+	}
+	return nil
+}
--- resource/space_resource.go
+++ resource/space_resource.go
@@ -25,6 +25,7 @@ func NewSpaceResource(db application.DB) SpaceResource {
 	}
 }
 
+//GetByID gets a space resource by its ID
 func (r SpaceResource) GetByID(ctx *gin.Context) {
 	spaceID, err := uuid.FromString(ctx.Param("spaceID"))
 	if err != nil {
@@ -33,20 +34,20 @@ func (r SpaceResource) GetByID(ctx *gin.Context) {
 	s, err := r.SpaceRepository.Load(ctx, spaceID)
 	if err != nil {
 		//TODO: retrieve the correct HTTP status for the given err
-		ctx.AbortWithError(http.StatusInternalServerError, errs.Wrapf(err, "error while fetching the space with id=%v", spaceID.String()))
+		ctx.AbortWithError(http.StatusInternalServerError, errs.Wrapf(err, "error while fetching the space with id=%s", spaceID.String()))
 	}
 	// convert the business-domain 'space' into a jsonapi-model space
 	result := model.Space{
 		ID:          s.ID.String(),
-		Version:     s.Version,
 		Name:        s.Name,
 		Description: s.Description,
+		BackLogSize: 10,
 	}
 
 	// marshall the result into a JSON-API compliant doc
 	ctx.Status(http.StatusOK)
 	ctx.Header("Content-Type", jsonapi.MediaType)
 	if err := jsonapi.MarshalPayload(ctx.Writer, &result); err != nil {
-		ctx.AbortWithError(http.StatusInternalServerError, errs.Wrapf(err, "error while fetching the space with id=", spaceID.String()))
+		ctx.AbortWithError(http.StatusInternalServerError, errs.Wrapf(err, "error while fetching the space with id=%s", spaceID.String()))
 	}
 }
