9ac2790cca9201cf68d7f81fc25e0f678774a1d6
Ratnadeep Debnath <rtnpro@gmail.com>
2017-08-31 18:15:42 +0530

    Added test cases for API redirection for POST, PATCH, DELETE

--- api/gin_engine_setup_test.go
+++ api/gin_engine_setup_test.go
@@ -1,6 +1,7 @@
 package api_test
 
 import (
+	"bytes"
 	"context"
 	"net/http"
 
@@ -39,11 +40,58 @@ var _ = Describe("Engine", func() {
 	})
 
 	Describe("Test API redirect", func() {
-		Context("Test API redirect", func() {
+		Context("Test API GET redirect", func() {
 
-			Specify("Test API redirect", func() {
+			Specify("Test API GET redirect", func() {
 				// given
 				r, _ := http.NewRequest(http.MethodGet, "/api/foobar", nil)
+
+				// when
+				rr := Execute(s.GinkgoTestSuite, r)
+
+				// then
+				assert.Equal(GinkgoT(), http.StatusTemporaryRedirect, rr.Code)
+				assert.Equal(GinkgoT(), "/legacyapi/foobar", rr.HeaderMap.Get("Location"))
+			})
+
+		})
+		Context("Test API POST redirect", func() {
+
+			Specify("Test API POST redirect", func() {
+				// given
+				payload := bytes.NewBuffer(make([]byte, 0))
+				r, _ := http.NewRequest(http.MethodPost, "/api/foobar", payload)
+
+				// when
+				rr := Execute(s.GinkgoTestSuite, r)
+
+				// then
+				assert.Equal(GinkgoT(), http.StatusTemporaryRedirect, rr.Code)
+				assert.Equal(GinkgoT(), "/legacyapi/foobar", rr.HeaderMap.Get("Location"))
+			})
+
+		})
+		Context("Test API PATCH redirect", func() {
+
+			Specify("Test API PATCH redirect", func() {
+				// given
+				payload := bytes.NewBuffer(make([]byte, 0))
+				r, _ := http.NewRequest(http.MethodPatch, "/api/foobar", payload)
+
+				// when
+				rr := Execute(s.GinkgoTestSuite, r)
+
+				// then
+				assert.Equal(GinkgoT(), http.StatusTemporaryRedirect, rr.Code)
+				assert.Equal(GinkgoT(), "/legacyapi/foobar", rr.HeaderMap.Get("Location"))
+			})
+
+		})
+		Context("Test API DELETE redirect", func() {
+
+			Specify("Test API DELETE redirect", func() {
+				// given
+				r, _ := http.NewRequest(http.MethodDelete, "/api/foobar", nil)
 
 				// when
 				rr := Execute(s.GinkgoTestSuite, r)
