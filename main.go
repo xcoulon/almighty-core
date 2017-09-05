fabric8-services/fabric8-wit

Resolve che namespace from tenant service  (#1593)
Related to openshiftio/openshift.io#619

main.go
@@ -332,6 +332,7 @@ func main() {
 
 	// Mount "codebase" controller
 	codebaseCtrl := controller.NewCodebaseController(service, appDB, configuration)
+	codebaseCtrl.ShowTenant = account.NewShowTenant(configuration)
 	app.MountCodebaseController(service, codebaseCtrl)
 
 	// Mount "spacecodebases" controller

xcoulon/almighty-core

main.go
@@ -264,6 +264,7 @@ func main() {
 
 	// Mount "codebase" controller
 	codebaseCtrl := controller.NewCodebaseController(service, appDB, config)
+	codebaseCtrl.ShowTenant = account.NewShowTenant(config)
 	app.MountCodebaseController(service, codebaseCtrl)
 
 	// Mount "spacecodebases" controller