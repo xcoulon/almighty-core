fabric8-services/fabric8-wit

Delegate Keycloak Resource creation to Auth service (#1589)

auth/policy.go
@@ -6,6 +6,17 @@ import (
 	"github.com/goadesign/goa"
 )
 
+// KeycloakConfiguration represents a keycloak configuration
+type KeycloakConfiguration interface {
+	GetKeycloakEndpointAuthzResourceset(*goa.RequestData) (string, error)
+	GetKeycloakEndpointToken(*goa.RequestData) (string, error)
+	GetKeycloakEndpointClients(*goa.RequestData) (string, error)
+	GetKeycloakEndpointAdmin(*goa.RequestData) (string, error)
+	GetKeycloakEndpointEntitlement(*goa.RequestData) (string, error)
+	GetKeycloakClientID() string
+	GetKeycloakSecret() string
+}
+
 // AuthzPolicyManager represents a space collaborators policy manager
 type AuthzPolicyManager interface {
 	GetPolicy(ctx context.Context, request *goa.RequestData, policyID string) (*KeycloakPolicy, *string, error)
@@ -71,4 +82,16 @@ func (m *KeycloakPolicyManager) UpdatePolicy(ctx context.Context, request *goa.R
 	}
 
 	return UpdatePolicy(ctx, clientsEndpoint, clientID, policy, pat)
+}
+
+func getPat(ctx context.Context, requestData *goa.RequestData, config KeycloakConfiguration) (string, error) {
+	endpoint, err := config.GetKeycloakEndpointToken(requestData)
+	if err != nil {
+		return "", err
+	}
+	token, err := GetProtectedAPIToken(ctx, endpoint, config.GetKeycloakClientID(), config.GetKeycloakSecret())
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }

controller/collaborators_blackbox_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/fabric8-services/fabric8-wit/app/test"
 	"github.com/fabric8-services/fabric8-wit/application"
 	"github.com/fabric8-services/fabric8-wit/auth"
+	config "github.com/fabric8-services/fabric8-wit/configuration"
 	. "github.com/fabric8-services/fabric8-wit/controller"
 	"github.com/fabric8-services/fabric8-wit/errors"
 	"github.com/fabric8-services/fabric8-wit/gormapplication"
@@ -71,6 +72,22 @@ func (m *DummyPolicyManager) RemoveUserFromPolicy(p *auth.KeycloakPolicy, userID
 	return p.RemoveUserFromPolicy(userID)
 }
 
+type dummyCollaboratorsConfiguration struct {
+	configuration *config.ConfigurationData
+}
+
+func (c *dummyCollaboratorsConfiguration) GetKeycloakEndpointEntitlement(*goa.RequestData) (string, error) {
+	return "", nil
+}
+
+func (c *dummyCollaboratorsConfiguration) GetCacheControlCollaborators() string {
+	return c.configuration.GetCacheControlCollaborators()
+}
+
+func (c *dummyCollaboratorsConfiguration) IsAuthorizationEnabled() bool {
+	return true
+}
+
 type TestCollaboratorsREST struct {
 	gormtestsupport.DBTestSuite
 
@@ -81,6 +98,7 @@ type TestCollaboratorsREST struct {
 	testIdentity2 account.Identity
 	testIdentity3 account.Identity
 	spaceID       uuid.UUID
+	authzConfig   CollaboratorsConfiguration
 }
 
 func TestRunCollaboratorsREST(t *testing.T) {
@@ -91,6 +109,7 @@ func TestRunCollaboratorsREST(t *testing.T) {
 func (rest *TestCollaboratorsREST) SetupTest() {
 	rest.db = gormapplication.NewGormDB(rest.DB)
 	rest.clean = cleaner.DeleteCreatedEntities(rest.DB)
+	rest.authzConfig = &dummyCollaboratorsConfiguration{rest.Configuration}
 
 	rest.policy = &auth.KeycloakPolicy{
 		Name:             "TestCollaborators-" + uuid.NewV4().String(),
@@ -118,12 +137,12 @@ func (rest *TestCollaboratorsREST) TearDownTest() {
 func (rest *TestCollaboratorsREST) SecuredController() (*goa.Service, *CollaboratorsController) {
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity1, &DummySpaceAuthzService{rest})
-	return svc, NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	return svc, NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 }
 
 func (rest *TestCollaboratorsREST) UnSecuredController() (*goa.Service, *CollaboratorsController) {
 	svc := goa.New("Collaborators-Service")
-	return svc, NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	return svc, NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 }
 
 func (rest *TestCollaboratorsREST) TestListCollaboratorsWithRandomSpaceIDNotFound() {
@@ -404,7 +423,7 @@ func (rest *TestCollaboratorsREST) TestRemoveCollaboratorsUnauthorizedIfCurrentU
 	// given
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity2, &DummySpaceAuthzService{rest})
-	ctrl := NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	ctrl := NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 	rest.policy.AddUserToPolicy(rest.testIdentity1.ID.String())
 	_, actualUsers := test.ListCollaboratorsOK(rest.T(), svc.Context, svc, ctrl, rest.spaceID, nil, nil, nil, nil)
 	rest.checkCollaborators([]uuid.UUID{rest.testIdentity1.ID}, actualUsers)
@@ -416,7 +435,7 @@ func (rest *TestCollaboratorsREST) TestRemoveManyCollaboratorsUnauthorizedIfCurr
 	// given
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity2, &DummySpaceAuthzService{rest})
-	ctrl := NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	ctrl := NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 	rest.policy.AddUserToPolicy(rest.testIdentity1.ID.String())
 	_, actualUsers := test.ListCollaboratorsOK(rest.T(), svc.Context, svc, ctrl, rest.spaceID, nil, nil, nil, nil)
 	rest.checkCollaborators([]uuid.UUID{rest.testIdentity1.ID}, actualUsers)

controller/space.go
@@ -45,11 +45,11 @@ type SpaceController struct {
 	*goa.Controller
 	db              application.DB
 	config          SpaceConfiguration
-	resourceManager auth.AuthzResourceManager
+	resourceManager auth.ResourceManager
 }
 
 // NewSpaceController creates a space controller.
-func NewSpaceController(service *goa.Service, db application.DB, config SpaceConfiguration, resourceManager auth.AuthzResourceManager) *SpaceController {
+func NewSpaceController(service *goa.Service, db application.DB, config SpaceConfiguration, resourceManager auth.ResourceManager) *SpaceController {
 	return &SpaceController{Controller: service.NewController("SpaceController"), db: db, config: config, resourceManager: resourceManager}
 }
 
@@ -126,14 +126,17 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 	}
 
 	// Create keycloak resource for this space
-	resource, err := c.resourceManager.CreateResource(ctx, ctx.RequestData, spaceID.String(), spaceResourceType, &spaceName, &scopes, currentUser.String())
+
+	resource, err := c.resourceManager.CreateSpace(ctx, ctx.RequestData, spaceID.String())
 	if err != nil {
+		// Unable to create a space resource. Can't proceed. Rool back space creation and return an error.
+		c.rollBackSpaceCreation(ctx, spaceID)
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
 	spaceResource := &space.Resource{
-		ResourceID:   resource.ResourceID,
-		PolicyID:     resource.PolicyID,
-		PermissionID: resource.PermissionID,
+		ResourceID:   resource.Data.ResourceID,
+		PolicyID:     resource.Data.PolicyID,
+		PermissionID: resource.Data.PermissionID,
 		SpaceID:      spaceID,
 	}
 
@@ -143,6 +146,15 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 		return err
 	})
 	if err != nil {
+		// Rool back space and space resource creation
+		c.rollBackSpaceCreation(ctx, spaceID)
+		remoteErr := c.resourceManager.DeleteSpace(ctx, ctx.RequestData, spaceID.String())
+		if remoteErr != nil {
+			log.Error(ctx, map[string]interface{}{
+				"err":      remoteErr,
+				"space_id": spaceID,
+			}, "unable to roll back space resource creation")
+		}
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
 
@@ -155,6 +167,19 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 	}
 	ctx.ResponseData.Header().Set("Location", rest.AbsoluteURL(ctx.RequestData, app.SpaceHref(res.Data.ID)))
 	return ctx.Created(res)
+}
+
+func (c *SpaceController) rollBackSpaceCreation(ctx context.Context, spaceID uuid.UUID) error {
+	err := application.Transactional(c.db, func(appl application.Application) error {
+		return appl.Spaces().Delete(ctx, spaceID)
+	})
+	if err != nil {
+		log.Error(ctx, map[string]interface{}{
+			"err":      err,
+			"space_id": spaceID,
+		}, "unable to roll back space creation")
+	}
+	return err
 }
 
 // Delete runs the delete action.
@@ -200,7 +225,7 @@ func (c *SpaceController) Delete(ctx *app.DeleteSpaceContext) error {
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
-	c.resourceManager.DeleteResource(ctx, ctx.RequestData, auth.Resource{ResourceID: resourceID, PermissionID: permissionID, PolicyID: policyID})
+	err = c.resourceManager.DeleteSpace(ctx, ctx.RequestData, ctx.SpaceID.String())
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}

controller/space_blackbox_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/fabric8-services/fabric8-wit/account"
 	"github.com/fabric8-services/fabric8-wit/app"
 	"github.com/fabric8-services/fabric8-wit/app/test"
-	"github.com/fabric8-services/fabric8-wit/auth"
+	"github.com/fabric8-services/fabric8-wit/auth/authservice"
 	"github.com/fabric8-services/fabric8-wit/configuration"
 	. "github.com/fabric8-services/fabric8-wit/controller"
 	"github.com/fabric8-services/fabric8-wit/gormapplication"
@@ -32,11 +32,11 @@ var spaceConfiguration *configuration.ConfigurationData
 type DummyResourceManager struct {
 }
 
-func (m *DummyResourceManager) CreateResource(ctx context.Context, request *goa.RequestData, name string, rType string, uri *string, scopes *[]string, userID string) (*auth.Resource, error) {
-	return &auth.Resource{ResourceID: uuid.NewV4().String(), PermissionID: uuid.NewV4().String(), PolicyID: uuid.NewV4().String()}, nil
+func (m *DummyResourceManager) CreateSpace(ctx context.Context, request *goa.RequestData, spaceID string) (*authservice.SpaceResource, error) {
+	return &authservice.SpaceResource{Data: &authservice.SpaceResourceData{ResourceID: uuid.NewV4().String(), PermissionID: uuid.NewV4().String(), PolicyID: uuid.NewV4().String()}}, nil
 }
 
-func (m *DummyResourceManager) DeleteResource(ctx context.Context, request *goa.RequestData, resource auth.Resource) error {
+func (m *DummyResourceManager) DeleteSpace(ctx context.Context, request *goa.RequestData, spaceID string) error {
 	return nil
 }
 

main.go
@@ -270,7 +270,7 @@ func main() {
 	}
 
 	// Mount "space" controller
-	spaceCtrl := controller.NewSpaceController(service, appDB, configuration, auth.NewKeycloakResourceManager(configuration))
+	spaceCtrl := controller.NewSpaceController(service, appDB, configuration, auth.NewAuthzResourceManager(configuration))
 	app.MountSpaceController(service, spaceCtrl)
 
 	// Mount "user" controller

xcoulon/almighty-core

auth/policy.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+// KeycloakConfiguration represents a keycloak configuration
+type KeycloakConfiguration interface {
+	GetKeycloakEndpointAuthzResourceset(*http.Request) (string, error)
+	GetKeycloakEndpointToken(*http.Request) (string, error)
+	GetKeycloakEndpointClients(*http.Request) (string, error)
+	GetKeycloakEndpointAdmin(*http.Request) (string, error)
+	GetKeycloakEndpointEntitlement(*http.Request) (string, error)
+	GetKeycloakClientID() string
+	GetKeycloakSecret() string
+}
+
 // AuthzPolicyManager represents a space collaborators policy manager
 type AuthzPolicyManager interface {
 	GetPolicy(ctx context.Context, request *http.Request, policyID string) (*KeycloakPolicy, *string, error)
@@ -70,4 +81,16 @@ func (m *KeycloakPolicyManager) UpdatePolicy(ctx context.Context, request *http.
 	}
 
 	return UpdatePolicy(ctx, clientsEndpoint, clientID, policy, pat)
+}
+
+func getPat(ctx context.Context, requestData *http.Request, config KeycloakConfiguration) (string, error) {
+	endpoint, err := config.GetKeycloakEndpointToken(requestData)
+	if err != nil {
+		return "", err
+	}
+	token, err := GetProtectedAPIToken(ctx, endpoint, config.GetKeycloakClientID(), config.GetKeycloakSecret())
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }

controller/collaborators_blackbox_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/fabric8-services/fabric8-wit/app/test"
 	"github.com/fabric8-services/fabric8-wit/application"
 	"github.com/fabric8-services/fabric8-wit/auth"
+	config "github.com/fabric8-services/fabric8-wit/configuration"
 	. "github.com/fabric8-services/fabric8-wit/controller"
 	"github.com/fabric8-services/fabric8-wit/errors"
 	"github.com/fabric8-services/fabric8-wit/gormapplication"
@@ -72,6 +73,22 @@ func (m *DummyPolicyManager) RemoveUserFromPolicy(p *auth.KeycloakPolicy, userID
 	return p.RemoveUserFromPolicy(userID)
 }
 
+type dummyCollaboratorsConfiguration struct {
+	configuration *config.ConfigurationData
+}
+
+func (c *dummyCollaboratorsConfiguration) GetKeycloakEndpointEntitlement(*http.Request) (string, error) {
+	return "", nil
+}
+
+func (c *dummyCollaboratorsConfiguration) GetCacheControlCollaborators() string {
+	return c.configuration.GetCacheControlCollaborators()
+}
+
+func (c *dummyCollaboratorsConfiguration) IsAuthorizationEnabled() bool {
+	return true
+}
+
 type TestCollaboratorsREST struct {
 	gormtestsupport.DBTestSuite
 
@@ -82,6 +99,7 @@ type TestCollaboratorsREST struct {
 	testIdentity2 account.Identity
 	testIdentity3 account.Identity
 	spaceID       uuid.UUID
+	authzConfig   CollaboratorsConfiguration
 }
 
 func TestRunCollaboratorsREST(t *testing.T) {
@@ -92,6 +110,7 @@ func TestRunCollaboratorsREST(t *testing.T) {
 func (rest *TestCollaboratorsREST) SetupTest() {
 	rest.db = gormapplication.NewGormDB(rest.DB)
 	rest.clean = cleaner.DeleteCreatedEntities(rest.DB)
+	rest.authzConfig = &dummyCollaboratorsConfiguration{rest.Configuration}
 
 	rest.policy = &auth.KeycloakPolicy{
 		Name:             "TestCollaborators-" + uuid.NewV4().String(),
@@ -119,12 +138,12 @@ func (rest *TestCollaboratorsREST) TearDownTest() {
 func (rest *TestCollaboratorsREST) SecuredController() (*goa.Service, *CollaboratorsController) {
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity1, &DummySpaceAuthzService{rest})
-	return svc, NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	return svc, NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 }
 
 func (rest *TestCollaboratorsREST) UnSecuredController() (*goa.Service, *CollaboratorsController) {
 	svc := goa.New("Collaborators-Service")
-	return svc, NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	return svc, NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 }
 
 func (rest *TestCollaboratorsREST) TestListCollaboratorsWithRandomSpaceIDNotFound() {
@@ -405,7 +424,7 @@ func (rest *TestCollaboratorsREST) TestRemoveCollaboratorsUnauthorizedIfCurrentU
 	// given
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity2, &DummySpaceAuthzService{rest})
-	ctrl := NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	ctrl := NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 	rest.policy.AddUserToPolicy(rest.testIdentity1.ID.String())
 	_, actualUsers := test.ListCollaboratorsOK(rest.T(), svc.Context, svc, ctrl, rest.spaceID, nil, nil, nil, nil)
 	rest.checkCollaborators([]uuid.UUID{rest.testIdentity1.ID}, actualUsers)
@@ -417,7 +436,7 @@ func (rest *TestCollaboratorsREST) TestRemoveManyCollaboratorsUnauthorizedIfCurr
 	// given
 	priv, _ := wittoken.ParsePrivateKey([]byte(wittoken.RSAPrivateKey))
 	svc := testsupport.ServiceAsSpaceUser("Collaborators-Service", wittoken.NewManagerWithPrivateKey(priv), rest.testIdentity2, &DummySpaceAuthzService{rest})
-	ctrl := NewCollaboratorsController(svc, rest.db, rest.Configuration, &DummyPolicyManager{rest: rest})
+	ctrl := NewCollaboratorsController(svc, rest.db, rest.authzConfig, &DummyPolicyManager{rest: rest})
 	rest.policy.AddUserToPolicy(rest.testIdentity1.ID.String())
 	_, actualUsers := test.ListCollaboratorsOK(rest.T(), svc.Context, svc, ctrl, rest.spaceID, nil, nil, nil, nil)
 	rest.checkCollaborators([]uuid.UUID{rest.testIdentity1.ID}, actualUsers)

controller/space.go
@@ -46,11 +46,11 @@ type SpaceController struct {
 	*goa.Controller
 	db              application.DB
 	config          SpaceConfiguration
-	resourceManager auth.AuthzResourceManager
+	resourceManager auth.ResourceManager
 }
 
 // NewSpaceController creates a space controller.
-func NewSpaceController(service *goa.Service, db application.DB, config SpaceConfiguration, resourceManager auth.AuthzResourceManager) *SpaceController {
+func NewSpaceController(service *goa.Service, db application.DB, config SpaceConfiguration, resourceManager auth.ResourceManager) *SpaceController {
 	return &SpaceController{Controller: service.NewController("SpaceController"), db: db, config: config, resourceManager: resourceManager}
 }
 
@@ -127,14 +127,17 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 	}
 
 	// Create keycloak resource for this space
-	resource, err := c.resourceManager.CreateResource(ctx, ctx.Request, spaceID.String(), spaceResourceType, &spaceName, &scopes, currentUser.String())
+
+	resource, err := c.resourceManager.CreateSpace(ctx, ctx.Request, spaceID.String())
 	if err != nil {
+		// Unable to create a space resource. Can't proceed. Rool back space creation and return an error.
+		c.rollBackSpaceCreation(ctx, spaceID)
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
 	spaceResource := &space.Resource{
-		ResourceID:   resource.ResourceID,
-		PolicyID:     resource.PolicyID,
-		PermissionID: resource.PermissionID,
+		ResourceID:   resource.Data.ResourceID,
+		PolicyID:     resource.Data.PolicyID,
+		PermissionID: resource.Data.PermissionID,
 		SpaceID:      spaceID,
 	}
 
@@ -144,6 +147,15 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 		return err
 	})
 	if err != nil {
+		// Rool back space and space resource creation
+		c.rollBackSpaceCreation(ctx, spaceID)
+		remoteErr := c.resourceManager.DeleteSpace(ctx, ctx.Request, spaceID.String())
+		if remoteErr != nil {
+			log.Error(ctx, map[string]interface{}{
+				"err":      remoteErr,
+				"space_id": spaceID,
+			}, "unable to roll back space resource creation")
+		}
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
 
@@ -156,6 +168,19 @@ func (c *SpaceController) Create(ctx *app.CreateSpaceContext) error {
 	}
 	ctx.ResponseData.Header().Set("Location", rest.AbsoluteURL(ctx.Request, app.SpaceHref(res.Data.ID)))
 	return ctx.Created(res)
+}
+
+func (c *SpaceController) rollBackSpaceCreation(ctx context.Context, spaceID uuid.UUID) error {
+	err := application.Transactional(c.db, func(appl application.Application) error {
+		return appl.Spaces().Delete(ctx, spaceID)
+	})
+	if err != nil {
+		log.Error(ctx, map[string]interface{}{
+			"err":      err,
+			"space_id": spaceID,
+		}, "unable to roll back space creation")
+	}
+	return err
 }
 
 // Delete runs the delete action.
@@ -201,7 +226,7 @@ func (c *SpaceController) Delete(ctx *app.DeleteSpaceContext) error {
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
-	c.resourceManager.DeleteResource(ctx, ctx.Request, auth.Resource{ResourceID: resourceID, PermissionID: permissionID, PolicyID: policyID})
+	err = c.resourceManager.DeleteSpace(ctx, ctx.Request, ctx.SpaceID.String())
 	if err != nil {
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}

controller/space_blackbox_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/fabric8-services/fabric8-wit/account"
 	"github.com/fabric8-services/fabric8-wit/app"
 	"github.com/fabric8-services/fabric8-wit/app/test"
-	"github.com/fabric8-services/fabric8-wit/auth"
+	"github.com/fabric8-services/fabric8-wit/auth/authservice"
 	"github.com/fabric8-services/fabric8-wit/configuration"
 	. "github.com/fabric8-services/fabric8-wit/controller"
 	"github.com/fabric8-services/fabric8-wit/gormapplication"
@@ -33,11 +33,11 @@ var spaceConfiguration *configuration.ConfigurationData
 type DummyResourceManager struct {
 }
 
-func (m *DummyResourceManager) CreateResource(ctx context.Context, request *http.Request, name string, rType string, uri *string, scopes *[]string, userID string) (*auth.Resource, error) {
-	return &auth.Resource{ResourceID: uuid.NewV4().String(), PermissionID: uuid.NewV4().String(), PolicyID: uuid.NewV4().String()}, nil
+func (m *DummyResourceManager) CreateSpace(ctx context.Context, request *http.Request, spaceID string) (*authservice.SpaceResource, error) {
+	return &authservice.SpaceResource{Data: &authservice.SpaceResourceData{ResourceID: uuid.NewV4().String(), PermissionID: uuid.NewV4().String(), PolicyID: uuid.NewV4().String()}}, nil
 }
 
-func (m *DummyResourceManager) DeleteResource(ctx context.Context, request *http.Request, resource auth.Resource) error {
+func (m *DummyResourceManager) DeleteSpace(ctx context.Context, request *http.Request, spaceID string) error {
 	return nil
 }
 

main.go
@@ -201,7 +201,7 @@ func main() {
 	}
 
 	// Mount "space" controller
-	spaceCtrl := controller.NewSpaceController(service, appDB, config, auth.NewKeycloakResourceManager(config))
+	spaceCtrl := controller.NewSpaceController(service, appDB, config, auth.NewAuthzResourceManager(config))
 	app.MountSpaceController(service, spaceCtrl)
 
 	// Mount "user" controller